fdec57488f29aa0caf31819b19bf519d839d49c3
Robert Szulist <r.szulist@gmail.com>
2022-10-09 00:58:23 +0200

    Make lists interactive

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main() {
 
 	go t.RunServer()
 
-	// var valueStore map[string]*cache.HostValueCache
 	valueStore := make(map[string]*server.HostValueCache)
 
 	for name, items := range *t.Monitoring.GetHosts() {
@@ -35,48 +34,49 @@ func main() {
 		}
 	}()
 
-	hostlist := tui.NewHostList(t.Monitoring.GetHostnames())
+	hostlist := tui.NewList("Hosts", t.Monitoring.GetHostnames())
 
-	grid := ui.NewGrid()
-	termWidth, termHeight := ui.TerminalDimensions()
-	grid.SetRect(0, 0, termWidth, termHeight)
+	a := t.Monitoring.GetKeys(hostlist.Rows[hostlist.SelectedRow])
 
-	// r := ui.NewRow(0.5, p1)
-	table1 := tui.NewTable("system.cpu.load")
-	table2 := tui.NewTable("system.sw.os")
+	useri := tui.NewUserInterface()
+	useri.HostList = hostlist
+	useri.MetricList = tui.NewList("Metrics", a)
+	useri.SetGrid()
 
-	grid.Set(
-		ui.NewRow(1.0,
-			ui.NewCol(0.1,
-				ui.NewRow(0.5, hostlist),
-			),
-			ui.NewCol(0.9,
-				ui.NewRow(0.5, table1),
-				ui.NewRow(0.5, table2),
-			),
-		),
-	)
-
+	ui.Render(useri.Grid)
 	uiEvents := ui.PollEvents()
 	ticker := time.NewTicker(time.Second).C
 
+	ui.Render(useri.Grid)
+	activeList := useri.HostList
 	for {
 		select {
 		case e := <-uiEvents:
 			switch e.ID {
+			case "j", "<Down>":
+				activeList.ScrollDown()
+				if activeList == useri.HostList {
+					useri.MetricList.Rows = t.Monitoring.GetKeys(hostlist.Rows[hostlist.SelectedRow])
+				}
+			case "k", "<Up>":
+				activeList.ScrollUp()
+				if activeList == useri.HostList {
+					useri.MetricList.Rows = t.Monitoring.GetKeys(hostlist.Rows[hostlist.SelectedRow])
+				}
+			case "<Enter>":
+				if activeList == useri.HostList {
+					activeList = useri.MetricList
+				}
+			case "<Escape>":
+				activeList = useri.HostList
 			case "q", "<C-c>", "e":
 				return
 			}
 		case <-ticker:
 
-			vals := valueStore["LOBOT"].GetValues("system.cpu.load")
-			tui.UpdateTable(table1, vals)
-
-			vals2 := valueStore["LOBOT"].GetValues("system.sw.os")
-			tui.UpdateTable(table2, vals2)
-
-			ui.Render(grid)
+			// update active widget values
 		}
+		ui.Render(useri.Grid)
 	}
 
 }
